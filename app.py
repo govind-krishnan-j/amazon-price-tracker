726e28f19eae023a9a9c805e4a285cd0964d087c
Govind Krishnan <govindkrishnan44@gmail.com>
2026-06-07 19:46:13 +0530

    Switch to PostgreSQL for production

--- app.py
+++ app.py
@@ -15,7 +15,12 @@ load_dotenv()
 # --- App setup ---
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "devkey123")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tracker.db"
+database_url = os.getenv("DATABASE_URL", "sqlite:///tracker.db")
+
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
